a22a739d2f870669d90300711dec0e32eeaa94ad
Miguel Bernedo <miguel@kambista.com>
2023-09-21 17:34:18 -0500

    Modified

--- Js/app.js
+++ Js/app.js
@@ -1,50 +1,52 @@
 let h1 = document.getElementById("Titulo");
 let Boton1 = document.getElementById("B1");
 
-document.getElementById("BotonCerrar").addEventListener('click', function() {
-    const ContenedorBotones = document.querySelector(".Con");
-    ContenedorBotones.style.display = "none";
-    document.querySelector(".Texto").style.display = "block";
-    CrearVarias();
-    h1.remove();
+document.getElementById("BotonCerrar").addEventListener('click', function () {
+  const ContenedorBotones = document.querySelector(".Con");
+  ContenedorBotones.style.display = "none";
+  const audio = new Audio('cancion.mp3');
+  audio.play();
+  document.querySelector(".Texto").style.display = "block";
+  CrearVarias();
+  h1.remove();
 })
 
 const canvas = document.getElementById('Flor');
 const ctx = canvas.getContext('2d');
 
 function DibujarPetalo(x, y, RadioX, scala, Rotacion, color, pasos) {
-    const Numero = scala;
+  const Numero = scala;
 
-    const AnguloIncrement = (Math.PI / pasos) * 2;
-    ctx.save();
-    ctx.translate(x, y);
-    ctx.rotate(Rotacion);
-    ctx.scale(1, Numero);
-    ctx.beginPath();
-    for (let i = 0; i <= pasos; i++) {
-        const AnguloActual = i * AnguloIncrement;
-        const currentRadius = Math.sin(AnguloActual) * RadioX;
-        const PuntoY = Math.sin(AnguloActual) * currentRadius;
-        const PuntoX = Math.cos(AnguloActual) * currentRadius;
-        if (i === 0) {
-          ctx.moveTo(PuntoX, PuntoY);
-        } else {
-          ctx.lineTo(PuntoX, PuntoY);
-        }
-        ctx.strokeStyle = color;
-        ctx.fillStyle = color;
-        ctx.fill();
-        ctx.stroke();
-      }
-    
-      ctx.restore();
+  const AnguloIncrement = (Math.PI / pasos) * 2;
+  ctx.save();
+  ctx.translate(x, y);
+  ctx.rotate(Rotacion);
+  ctx.scale(1, Numero);
+  ctx.beginPath();
+  for (let i = 0; i <= pasos; i++) {
+    const AnguloActual = i * AnguloIncrement;
+    const currentRadius = Math.sin(AnguloActual) * RadioX;
+    const PuntoY = Math.sin(AnguloActual) * currentRadius;
+    const PuntoX = Math.cos(AnguloActual) * currentRadius;
+    if (i === 0) {
+      ctx.moveTo(PuntoX, PuntoY);
+    } else {
+      ctx.lineTo(PuntoX, PuntoY);
+    }
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.stroke();
+  }
+
+  ctx.restore();
 }
 
 function DibujarFlor(x, y, NumeroPetalos, RadioXPetalo, RadioYPetalo, AltoTrazo) {
-    // Tallo
-    const PasosTallo = 50;
-    const AltoTallo = AltoTrazo / PasosTallo;
-    let NuevaY = y;
+  // Tallo
+  const PasosTallo = 50;
+  const AltoTallo = AltoTrazo / PasosTallo;
+  let NuevaY = y;
 
   const DibujarTallo = () => {
     if (NuevaY < y + AltoTrazo) {
@@ -75,30 +77,30 @@ function DibujarFlor(x, y, NumeroPetalos, RadioXPetalo, RadioYPetalo, AltoTrazo)
   };
   DibujarTallo();
 
-    const AnguloIncrement = (Math.PI * 2) / NumeroPetalos;
-  
-    let contadorPetalos = 0;
-    function dibujarSiguientePetalo() {
-        if (contadorPetalos <= NumeroPetalos) {
-          const Angulo = contadorPetalos * AnguloIncrement;
-          DibujarPetalo(x, y, RadioXPetalo, 2, Angulo, 'yellow', 100);
-          contadorPetalos++;
-          setTimeout(dibujarSiguientePetalo, 1000); 
-        }
-        // Dibuja el centro de la flor
-        ctx.beginPath();
-        ctx.arc(x, y, 10, 0, Math.PI * 2);
-        ctx.fillStyle = 'white';
-        ctx.fill();
-      }
-      dibujarSiguientePetalo();
+  const AnguloIncrement = (Math.PI * 2) / NumeroPetalos;
+
+  let contadorPetalos = 0;
+  function dibujarSiguientePetalo() {
+    if (contadorPetalos <= NumeroPetalos) {
+      const Angulo = contadorPetalos * AnguloIncrement;
+      DibujarPetalo(x, y, RadioXPetalo, 2, Angulo, 'yellow', 100);
+      contadorPetalos++;
+      setTimeout(dibujarSiguientePetalo, 1000);
+    }
+    // Dibuja el centro de la flor
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+  }
+  dibujarSiguientePetalo();
 }
 
 function DibujarFlorSinTallo(x, y, NumeroPetalos, RadioXPetalo, RadioYPetalo, AltoTrazo) {
-    // Dibuja el tallo
-    const PasosTallo = 50;
-    const AltoTallo = AltoTrazo / PasosTallo;
-    let NuevaY = y;
+  // Dibuja el tallo
+  const PasosTallo = 50;
+  const AltoTallo = AltoTrazo / PasosTallo;
+  let NuevaY = y;
 
   const DibujarTallo = () => {
     if (NuevaY < y + AltoTrazo) {
@@ -110,44 +112,44 @@ function DibujarFlorSinTallo(x, y, NumeroPetalos, RadioXPetalo, RadioYPetalo, Al
       ctx.stroke();
       NuevaY += AltoTallo;
       setTimeout(DibujarTallo, 100);
-    } 
+    }
   };
   DibujarTallo();
 
-    const AnguloIncrement = (Math.PI * 2) / NumeroPetalos;
-  
-    // Dibuja los pétalos
-    let contadorPetalos = 0;
-    function dibujarSiguientePetalo() {
-        if (contadorPetalos <= NumeroPetalos) {
-          const Angulo = contadorPetalos * AnguloIncrement;
-          DibujarPetalo(x, y, RadioXPetalo, 2, Angulo, 'yellow', 100);
-          contadorPetalos++;
-          setTimeout(dibujarSiguientePetalo, 1000); 
-        }
-        // Dibuja el centro de la flor
-        ctx.beginPath();
-        ctx.arc(x, y, 10, 0, Math.PI * 2);
-        ctx.fillStyle = 'white';
-        ctx.fill();
-      }
-      dibujarSiguientePetalo();
+  const AnguloIncrement = (Math.PI * 2) / NumeroPetalos;
+
+  // Dibuja los pétalos
+  let contadorPetalos = 0;
+  function dibujarSiguientePetalo() {
+    if (contadorPetalos <= NumeroPetalos) {
+      const Angulo = contadorPetalos * AnguloIncrement;
+      DibujarPetalo(x, y, RadioXPetalo, 2, Angulo, 'yellow', 100);
+      contadorPetalos++;
+      setTimeout(dibujarSiguientePetalo, 1000);
+    }
+    // Dibuja el centro de la flor
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+  }
+  dibujarSiguientePetalo();
 }
 
 function CrearVarias() {
-    const numFlores = 7;
+  const numFlores = 7;
 
-    // Espaciamiento y tamaño de cada flor
-    const espacioX = canvas.width / 4;
-    const espacioY = canvas.height / 3;
-    const TamañoFlor = 130;
+  // Espaciamiento y tamaño de cada flor
+  const espacioX = canvas.width / 4;
+  const espacioY = canvas.height / 3;
+  const TamañoFlor = 130;
 
-    for (let i = 0; i <= numFlores; i++) {
-        const fila = Math.floor(i / 4);
-        const columna = i % 4;
-        const x = espacioX * columna + espacioX / 2;
-        const y = espacioY * fila + espacioY / 2;
+  for (let i = 0; i <= numFlores; i++) {
+    const fila = Math.floor(i / 4);
+    const columna = i % 4;
+    const x = espacioX * columna + espacioX / 2;
+    const y = espacioY * fila + espacioY / 2;
 
-        DibujarFlorSinTallo(x, y, 8, 30, 80, TamañoFlor);
-    }
+    DibujarFlorSinTallo(x, y, 8, 30, 80, TamañoFlor);
+  }
 }
